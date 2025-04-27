f024137e9107a9e22b543702a9a4305f4f3f00fe
Liam Hong <wisehhong@gmail.com>
2025-04-26 17:49:56 -0700

    Tried to create Spawn and Destroy mechanics
    Tried to create Spawn and Destroy mechanics for Cachalot mission.

--- Assets/Scripts/CachScript.cs
+++ Assets/Scripts/CachScript.cs
@@ -1,17 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CachScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Start ()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        
+
     }
+    
 }
